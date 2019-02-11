716aa933292d7dae613d0a65f3f070398bade43f
Tyler <tyler@fitmentgroup.com>
2019-02-11 12:45:58 -0600

    Update formatting

--- lib/ymd.rb
+++ lib/ymd.rb
@@ -3,6 +3,6 @@ require 'date'
 
 module YMD
   def self.ymd(separator='')
-	  DateTime.now.strftime("%Y#{separator}%m#{separator}%d")
-	end
+    DateTime.now.strftime("%Y#{separator}%m#{separator}%d")
+  end
 end
